cf4e0907168c94f2f6a1b5ee162c87df87142559
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2026-07-31 14:24:48 +0200

    niri config imported

--- sessions/niri/home.nix
+++ sessions/niri/home.nix
@@ -1,7 +1,14 @@
-{ lib, osConfig, ... }:
+{ config, lib, osConfig, ... }:
 
+let
+  niriDotfiles =
+    "${config.home.homeDirectory}/nixos-config/dotfiles/niri";
+in
 {
   config = lib.mkIf osConfig.desktopSessions.niri.enable {
     desktopShell.dms.sessions = [ "niri" ];
+
+    xdg.configFile."niri".source =
+      config.lib.file.mkOutOfStoreSymlink niriDotfiles;
   };
 }
