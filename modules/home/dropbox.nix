02f9351c041afdf3a3c8cbcf3bda259fd94761fd
lgo <lgodlewski1987@gmail.com>
2026-08-06 20:30:14 +0200

    dropbox service

--- modules/home/dropbox.nix
+++ modules/home/dropbox.nix
@@ -1,5 +1,24 @@
 { pkgs, ... }:
 
 {
-  home.packages = with pkgs; [ dropbox ];
+  home.packages = [ pkgs.dropbox ];
+
+  systemd.user.services.dropbox = {
+    Unit = {
+      Description = "Dropbox desktop client";
+      After = [
+        "graphical-session.target"
+        "dms.service"
+      ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.dropbox}/bin/dropbox";
+      Restart = "on-failure";
+      RestartSec = 5;
+    };
+
+    Install.WantedBy = [ "graphical-session.target" ];
+  };
 }
