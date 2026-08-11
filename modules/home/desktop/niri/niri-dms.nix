9bdd81ac698857489c5e22dd8648dc1a13599525
Łukasz Godlewski <lgodlewski@itsg.com.pl>
2026-08-11 13:08:41 +0200

    simplify code

--- modules/home/desktop/niri/niri-dms.nix
+++ modules/home/desktop/niri/niri-dms.nix
@@ -1,6 +1,9 @@
 { config, ... }:
 
 {
+  imports = [
+    ../dms.nix
+  ];
   xdg.configFile."niri/dms".source =
     config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nixos-config/modules/home/desktop/niri/config/dms";
 }
--- profiles/niri.nix
+++ profiles/niri.nix
@@ -1,5 +1,16 @@
-{ pkgs, ... }:
+{ config, pkgs, ... }:
 
+let
+  screenshot = pkgs.writeShellApplication {
+    name = "screenshot";
+    runtimeInputs = with pkgs; [
+      coreutils
+      satty
+      config.programs.niri.package
+    ];
+    text = builtins.readFile ../modules/home/desktop/niri/screenshot;
+  };
+in
 {
   programs.niri.enable = true;
 
@@ -7,9 +18,22 @@
   # on-demand XWayland support for X11-only clients such as Dropbox's tray.
   environment.systemPackages = [ pkgs.xwayland-satellite ];
 
-  home-manager.users.lgo.imports = [
-    ../modules/home/desktop/niri
-    ../modules/home/desktop/niri/niri-dms.nix
-    ../modules/home/desktop/dms.nix
-  ];
+  home-manager.users.lgo =
+    { config, ... }:
+
+    let
+      niriConfigFile = "${config.home.homeDirectory}/nixos-config/modules/home/desktop/niri/config/config.kdl";
+      niriConfigCustomizationDir = "${config.home.homeDirectory}/nixos-config/modules/home/desktop/niri/config/custom";
+    in
+    {
+      imports = [
+        ../modules/home/desktop/niri/niri-dms.nix
+      ];
+      home.packages = [ screenshot ];
+
+      xdg.configFile."niri/config.kdl".source = config.lib.file.mkOutOfStoreSymlink niriConfigFile;
+      xdg.configFile."niri/custom".source =
+        config.lib.file.mkOutOfStoreSymlink niriConfigCustomizationDir;
+    };
+
 }
